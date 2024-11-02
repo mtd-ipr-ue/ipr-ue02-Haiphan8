738c19ffba55973262abfda0fe6e5370843aa5d2
Haiphan8 <stephan.haiden@aon.at>
2024-11-02 16:30:57 +0100

    2 finished

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,28 @@
 import libraries.Out;
-
+import libraries.In;
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
-    }
+        
+System.out.print("Geben Sie drei Ganze Zahlen ein: ");
+
+int a = In.readInt();
+int b = In.readInt();
+int c = In.readInt();
+
+
+if (a>b && a>c) {
+    System.out.print(a);
 }
+else if (c>a && c>b){
+
+    System.out.print(c);
+}
+
+else if (b>a && b>c){
+    System.out.print(b);
+}
+System.out.print(" ist die größte Zahl.");
+}}
+
+
+
--- src/ue02_2.java
+++ src/ue02_2.java
@@ -1,7 +1,37 @@
 import libraries.Out;
-
+import libraries.In;
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
-    }
+        Out.println("Geben Sie eine Jahreszahl und einen Monatswert zwischen 1 und 12 an:");
+    
+int year = In.readInt();
+int month = In.readInt();
+
+switch (month)
+    {case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+    System.out.println("31");
+    break;
+
+    case 4: case 6: case 9: case 11:
+    System.out.print("30");
+    break;
+
+
+    case 2:
+        
+        if (year%4 != 0 || year%100 == 0 && year%400 != 0 ) {
+            System.out.println("28");
+        }
+        else {
+            System.out.println("29");
+        }
+        break;
 }
+
+
+    
+
+}
+
+}
+    
